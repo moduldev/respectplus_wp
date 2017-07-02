c354629b7e05cd59b276fa56a09fffadcd632ac8
WIN-B7Q2GA7EON7\User <modul.work@gmail.com>
2017-07-02 21:03:47 +0300

    changed form

--- wp-content/themes/respectplus/page-sendorder.php
+++ wp-content/themes/respectplus/page-sendorder.php
@@ -8,9 +8,18 @@ Template Name: send order
         <h4>Форма для заявки.</h4>
         <div class="col-xs-12">
             <form action="send.php" class="form-group" method="post">
-                <div class="col-xs-12">
-                    <label for="">Меня зовут:<input name="customername" class="form-control" type="text" required></label>
-                    <label for="">Мой контактный номер:<input name="customerPhone" class="form-control" type="text" required></label>
+                <div class=" form-group col-xs-12">
+                    <label for="customername">Меня зовут:</label>
+                    <div class="input-group col-xs-6">
+                        <div class="input-group-addon"><i class="fa fa-user-o"></i></div>
+                        <input name="customername" id="customername" class="form-control" type="text" required>
+
+                    </div>
+                    <label for="customerPhone">Мой контактный номер:</label>
+                    <div class="input-group col-xs-6">
+                        <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+                        <input name="customerPhone" id="customerPhone" class="form-control" type="text" required>
+                    </div>
                 </div>
                 <div class="col-xs-4 ">
                     <p>Я хочу:</p>
@@ -31,19 +40,24 @@ Template Name: send order
                 <div class="col-xs-4">
                     <div class="checkbox">
 
-                        <label for=""><input name="estate_type[]" type="checkbox" value="1-k">1-к квартиру</label>
+                        <label for=""><input name="estate_type[]" type="checkbox" value="1-k">1-к
+                            квартиру</label>
                     </div>
                     <div class="checkbox">
-                        <label for=""><input name="estate_type[]" type="checkbox" value="2-k">2-к квартиру</label>
+                        <label for=""><input name="estate_type[]" type="checkbox" value="2-k">2-к
+                            квартиру</label>
                     </div>
                     <div class="checkbox">
-                        <label for=""><input name="estate_type[]" type="checkbox" value="3-k">3-к квартиру</label>
+                        <label for=""><input name="estate_type[]" type="checkbox" value="3-k">3-к
+                            квартиру</label>
                     </div>
                     <div class="checkbox">
-                        <label for=""><input name="estate_type[]" type="checkbox" value="4-k">4-к квартиру</label>
+                        <label for=""><input name="estate_type[]" type="checkbox" value="4-k">4-к
+                            квартиру</label>
                     </div>
                     <div class="checkbox">
-                        <label for=""><input name="estate_type[]" type="checkbox" value="5-k">5-к квартиру</label>
+                        <label for=""><input name="estate_type[]" type="checkbox" value="5-k">5-к
+                            квартиру</label>
                     </div>
                     <div class="checkbox">
                         <label for=""><input name="estate_type[]" type="checkbox" value="garage">гараж</label>
@@ -64,11 +78,14 @@ Template Name: send order
                         <label for=""><input name="estate_region[]" type="checkbox" value="kamenskom">Каменском</label>
                     </div>
                     <div class="checkbox">
-                        <label for="">Другой регион<br><input name="estate_region[]" type="text" value="" placeholder="Введите регион"></label>
+                        <label for="">Другой регион<br><input name="estate_region[]" type="text" value=""
+                                                              placeholder="Введите регион"></label>
                     </div>
                 </div>
                 <div class="col-xs-12">
-                    <button class="btn btn-default" style="display: block;" name="sendorder" type="submit">Отправить</button>
+                    <button class="btn btn-default" style="display: block;" name="sendorder" type="submit">
+                        Отправить
+                    </button>
                 </div>
             </form>
         </div>
